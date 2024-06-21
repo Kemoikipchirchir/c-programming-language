7e6f3e250af421e5e519f6e6088c7c3204c3800e
kemoikipchirchir <kemoikipchirchir@gmail.com>
2024-06-21 08:04:19 +0300

    open the folder

---
+++ Untitled-1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+float weight[5]={1,2,3,4,5};
+int sum =0;
+for(int i=0;i<5;i++){
+printf("weight[%f]"=%f\n,weight);
+sum =sum +weight;
+printf("the sum is %f",weight);
+}
+return 0;
+}
