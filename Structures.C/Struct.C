ebcd8712cc9ca2032ef7d8d9a37534b9ab5442a9
Kemoikipchirchir <kemoikipchirchir@gmail.com>
2024-07-10 11:59:57 +0300

    Update and rename Structures.C to Struct.C

--- Structures.C/Struct.C
+++ Structures.C/Struct.C
@@ -1,12 +1,12 @@
 c stuctures
 author =Edwin kipchirchir
 AdmNo=BSE-01-00072024
-Date=04072024
+Date=04/07/2024
 description=c structures
 #include stdio.h
 #includestring.h
 
-Define structure'student'
+//Define structure'student'
 struct student{
 char first_name[20];
 char last_name[20];
@@ -17,7 +17,7 @@ float gpa;
 }student1;
 
 int main(){
-initialize fields of a student
+//initialize fields of a student
 strcpy(student1.first_name,Edwin);
 strcpy(student1.last_name,kemoi);
 strcpy(student1.email,kemoikipchirchir@gmail.com);
@@ -25,7 +25,7 @@ student1.age=20;
 student1.phoneNo=757436193;
 student1.gpa=3.5;
 
-print the values of a student
+//print the values of a student
 printf(student1.first_name%sn,student1.first_name);
 printf(student1.last_name%sn,student1.last_name);
 printf(student1.email%sn,student1.email);
